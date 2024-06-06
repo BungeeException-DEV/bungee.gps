75b1e4728a6b31226460cc8071ddb196de6eb185
Cedrik Mutz <67883676+BungeeException-DEV@users.noreply.github.com>
2024-06-06 22:55:10 +0200

    Create config.lua

---
+++ shared/config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+Config.Jobs = {
+    ['police'] = { blipColor = 3 },
+    ['ambulance'] = { blipColor = 1 }
+}
+
+Config.Gangs = {
+    ['ballas'] = { blipColor = 83 },
+    ['vagos'] = { blipColor = 5 }
+}
+
+Config.ItemName = 'gps_tracker'
+Config.BlipUpdateInterval = 5000
