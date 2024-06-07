96bac2c1b5b9b366f942414f5f67a5b9b53171f6
Cedrik Mutz <67883676+BungeeException-DEV@users.noreply.github.com>
2024-06-07 02:33:32 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
-QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qb-core']:GetCoreObject()
 local groupBlip = nil
 
 local function updateGroupBlip(coords, blipColor, groupName)
@@ -7,9 +7,9 @@ local function updateGroupBlip(coords, blipColor, groupName)
         SetBlipSprite(groupBlip, 1)
         SetBlipAsShortRange(groupBlip, true)
         SetBlipDisplay(groupBlip, 4)
-        SetBlipPriority(groupBlip, 10) -- Blip Priority
-        SetBlipScale(groupBlip, 0.7) -- Blip Scale
-        SetBlipCategory(groupBlip, 7) -- That the blip is at the top
+        SetBlipPriority(groupBlip, 10)
+        SetBlipScale(groupBlip, 0.7)
+        SetBlipCategory(groupBlip, 7)
     end
     
     SetBlipCoords(groupBlip, coords.x, coords.y, coords.z)
@@ -19,7 +19,6 @@ local function updateGroupBlip(coords, blipColor, groupName)
     EndTextCommandSetBlipName(groupBlip)
 end
 
--- Check if the player has the item
 local function hasGpsTracker()
     local player = QBCore.Functions.GetPlayerData()
     for _, item in ipairs(player.items) do
@@ -30,31 +29,68 @@ local function hasGpsTracker()
     return false
 end
 
--- Main function to update the blips
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(Config.BlipUpdateInterval)
-
-        local player = QBCore.Functions.GetPlayerData()
-        if hasGpsTracker() then
-            QBCore.Functions.TriggerCallback('madv.gps:server:GetGroupBlipData', function(groupData)
-                if groupData then
-                    local coords = groupData.coords
-                    local blipColor = groupData.blipColor
-                    local groupName = groupData.groupName
-                    updateGroupBlip(coords, blipColor, groupName)
-                else
-                    if groupBlip then
-                        RemoveBlip(groupBlip)
-                        groupBlip = nil
-                    end
+local function updateBlip()
+    local player = QBCore.Functions.GetPlayerData()
+    if hasGpsTracker() then
+        QBCore.Functions.TriggerCallback('madv.gps:server:GetGroupBlipData', function(groupData)
+            if groupData then
+                local coords = groupData.coords
+                local blipColor = groupData.blipColor
+                local groupName = groupData.groupName
+                updateGroupBlip(coords, blipColor, groupName)
+            else
+                if groupBlip then
+                    RemoveBlip(groupBlip)
+                    groupBlip = nil
                 end
-            end, player.job.name, player.gang.name)
-        else
-            if groupBlip then
-                RemoveBlip(groupBlip)
-                groupBlip = nil
             end
+        end, player.job.name, player.gang.name)
+    else
+        if groupBlip then
+            RemoveBlip(groupBlip)
+            groupBlip = nil
         end
+    end
+end
+
+RegisterCommand('reloadgps', function()
+    updateBlip()
+    QBCore.Functions.Notify('GPS reloaded')
+end, false)
+
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function()
+    Citizen.CreateThread(function()
+        while true do
+            updateBlip()
+            Citizen.Wait(Config.BlipUpdateInterval)
+        end
+    end)
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    if groupBlip then
+        RemoveBlip(groupBlip)
+        groupBlip = nil
+    end
+end)
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate', function(JobInfo)
+    QBCore.Functions.GetPlayerData().job = JobInfo
+    updateBlip()
+end)
+
+RegisterNetEvent('QBCore:Client:OnGangUpdate', function(GangInfo)
+    QBCore.Functions.GetPlayerData().gang = GangInfo
+    updateBlip()
+end)
+
+AddEventHandler('onResourceStart', function(resourceName)
+    if GetCurrentResourceName() == resourceName then
+        Citizen.CreateThread(function()
+            while not QBCore.Functions.GetPlayerData().job do
+                Citizen.Wait(100)
+            end
+            updateBlip()
+        end)
     end
 end)
